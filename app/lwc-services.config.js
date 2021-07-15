85ba11b5a63a956415474bfedf52be104cb15052
jundu-net <50834433+jundu-net@users.noreply.github.com>
2021-07-15 00:28:00 +0000

    Lightning Base ComponentsとSLDSを適用

--- app/lwc-services.config.js
+++ app/lwc-services.config.js
@@ -1,5 +1,22 @@
 // Find the full example of all available configuration options at
 // https://github.com/muenzpraeger/create-lwc-app/blob/main/packages/lwc-services/example/lwc-services.config.js
 module.exports = {
-    resources: [{ from: 'src/resources/', to: 'dist/resources/' }]
+    resources: [
+        {
+            from: 'src/resources/',
+            to: 'dist/resources/'
+        },
+        // {
+        //     from: 'node_modules/@salesforce-ux/design-system/assets',
+        //     to: 'src/assets'
+        // },
+        {
+            from: 'node_modules/@salesforce-ux/design-system/assets',
+            to: 'dist/assets'
+        },
+        // {
+        //     from: 'node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css',
+        //     to: 'dist/assets/styles/salesforce-lightning-design-system.min.css'
+        // },
+    ]
 };
