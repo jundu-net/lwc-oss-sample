167701a2d31fa67d3fd53720df0de28f3bd2adcf
jundu-net <50834433+jundu-net@users.noreply.github.com>
2021-07-20 03:53:30 +0000

    Navigoの追加と、開発サーバの対応

--- app/lwc-services.config.js
+++ app/lwc-services.config.js
@@ -18,5 +18,16 @@ module.exports = {
         //     from: 'node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css',
         //     to: 'dist/assets/styles/salesforce-lightning-design-system.min.css'
         // },
-    ]
+    ],
+    devServer: {
+        historyApiFallback: {
+            index: 'index.html'
+        }
+        // port: 3001,
+        // host: 'localhost',
+        // open: false,
+        // stats: 'errors-only',
+        // noInfo: true,
+        // contentBase: './src/client'
+    },
 };
