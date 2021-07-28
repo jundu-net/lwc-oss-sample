ef4579f7be2201967171279f9344276165f84ff9
jundu-net <50834433+jundu-net@users.noreply.github.com>
2021-07-28 12:03:47 +0000

    表示と編集の基本動作を実装

--- app/src/modules/view/editForm/editForm.js
+++ app/src/modules/view/editForm/editForm.js
@@ -1,3 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class EditForm extends LightningElement { }
+export default class EditForm extends LightningElement {
+
+    @track formData = {
+        field1: '寿限無寿限無',
+        field2: 'じゅげむじゅげむ',
+    }
+
+    handleChange(evt) {
+        //console.log(locale);
+        let value;
+        if (evt.target.type === 'checkbox' || evt.target.type === 'checkbox-button' || evt.target.type === 'toggle') {
+            value = evt.target.checked;
+        } else {
+            value = evt.target.value;
+        }
+        // プロパティをidから探して、あれば値を自動セットすることも可能
+        switch (evt.target.dataset.id) {
+            case 'field1':
+                this.formData.field1 = value;
+                break;
+            case 'field2':
+                this.formData.field2 = value;
+                break;
+            default:
+                break;
+        }
+    }
+
+}
